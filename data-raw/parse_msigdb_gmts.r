32f08bb0481e32d30bb4cbc1ad59302f5bf79ec2
Casey Shannon <also.casey@gmail.com>
2015-09-25 18:37:05 -0700

    Update GMT parsing script to reflect change to MSigDB subcollections.

--- data-raw/parse_msigdb_gmts.r
+++ data-raw/parse_msigdb_gmts.r
@@ -1,8 +1,22 @@
 #' Source code for parsing MSigDB GMT files obtained from the Broad Institute.
-files <- list.files(path = ".", pattern = ".gmt$", full.names = T)
-names(files) <- c("BTM", "C1_positional", "C2_canonical", "C3_computational",
-                  "C4_computational", "C5_ontology", "C6_oncological",
-                  "C7_immunological", "H_hallmark")
+files <- list.files(path = "data-raw/", pattern = ".gmt$", full.names = T)
+names(files) <- c("BTM_ALL",
+                  "C1_positional_ALL",
+                  "C2_canonical_CGP",
+                  "C2_canonical_CP_BIOCARTA",
+                  "C2_canonical_CP_KEGG",
+                  "C2_canonical_CP_REACTOME",
+                  "C2_canonical_CP_PID",
+                  "C3_motif_MIR",
+                  "C3_motif_TFT",
+                  "C4_computational_CGN",
+                  "C4_computational_CN",
+                  "C5_ontology_BP",
+                  "C5_ontology_CC",
+                  "C5_ontology_MF",
+                  "C6_oncogenic_ALL",
+                  "C7_immunologic_ALL",
+                  "H_hallmark_ALL")
 
 # read in as list of lists
 tbl_genesets <- lapply(files, function(gmt){
@@ -12,8 +26,69 @@ tbl_genesets <- lapply(files, function(gmt){
   geneset <- lapply(geneset, function(x) tail(x, -2))
 })
 
+# clean up C2 collection - CP includes all other CP_x + CP_PID
+gsub("^(BIOCARTA|KEGG|REACTOME|PID).+", "\\1", names(tbl_genesets$C2_canonical_CP_PID)) %>%
+  table()
+
+# check to make sure all all included in respective sub-collections
+all(grep("KEGG", names(tbl_genesets$C2_canonical_CP_PID), value = T) %in% names(tbl_genesets$C2_canonical_CP_KEGG))
+all(grep("BIOCARTA", names(tbl_genesets$C2_canonical_CP_PID), value = T) %in% names(tbl_genesets$C2_canonical_CP_BIOCARTA))
+all(grep("REACTOME", names(tbl_genesets$C2_canonical_CP_PID), value = T) %in% names(tbl_genesets$C2_canonical_CP_REACTOME))
+
+# move others to new sub-collection
+tbl_genesets$C2_canonical_CP_OTHER <- tbl_genesets$C2_canonical_CP_PID[grep("BIOCARTA|KEGG|REACTOME|PID", names(tbl_genesets$C2_canonical_CP_PID), invert = T)]
+
+# keep only PID in PID sub-collection
+tbl_genesets$C2_canonical_CP_PID <- tbl_genesets$C2_canonical_CP_PID[grep("PID", names(tbl_genesets$C2_canonical_CP_PID))]
+
+# sort list by names
+tbl_genesets <- tbl_genesets[order(names(tbl_genesets))]
+
 # convert to list of data.frames
 tbl_genesets <- lapply(tbl_genesets, function(x) data.frame(geneset = names(x), members = I(x)))
 
 # convert to data.frame
 tbl_genesets <- dplyr::bind_rows(tbl_genesets, .id = "collection")
+
+# add sub-collections
+tbl_genesets$subcollection <- gsub("BTM_(ALL)|C[0-9]_[a-z]+_(.+)|H_[a-z]+_(ALL)", "\\1\\2\\3", tbl_genesets$collection)
+tbl_genesets$collection <- gsub("(BTM)_ALL|(C[0-9]_[a-z]+)_.+|(H_[a-z]+)_ALL", "\\1\\2\\3", tbl_genesets$collection)
+
+# reorder
+tbl_genesets <- dplyr::select(tbl_genesets, collection, subcollection, geneset, members)
+
+# add tissues
+tissues <- readRDS("data-raw/Tissues_tbl_df.rds")
+tissues_map <- readRDS("data-raw/Tissues_map_tbl_df.rds")
+all(tissues_map$celltype == tissues$geneset)
+tissues$subcollection <- tissues_map$category
+tissues <- dplyr::select(tissues, collection, subcollection, geneset, members)
+
+# final object
+genesets <- rbind(tbl_genesets, tissues)
+
+# creat mirna version of genesets
+# we'll use MSigDB C3 miRNA targets
+# only mir target gene sets
+mirs_annot <- genesets$C3_motif
+indeces <- names(mirs_annot) %>%
+  grep("^[ATGC]{7},.+", x = .)
+mirs_annot <- mirs_annot[indeces]
+
+getmirs <- function(geneset, mirs_annot) {
+  if(length(geneset) > 2000){
+    query <- paste(sample(geneset, 2000), collapse = "|")
+  } else {
+    query <- paste(geneset, collapse = "|")
+  }
+  names(mirs_annot)[grep(query, mirs_annot)] %>%
+    gsub("^[ATGC]{7},(.+)", "\\1", .) %>%
+    strsplit(",") %>%
+    unlist() %>%
+    unique()
+}
+
+# getmirs(references$BTM$`targets of FOSL1/2 (M0)`, mirs_annot = mirs_annot)
+references_sub_remapped <- rapply(references_sub[c(1,2,4,5,6)], how = "replace",
+                                  function(geneset) getmirs(geneset, mirs_annot))
+
