9e3b2cc521887676a3727c836493229d921bc797
Casey Shannon <also.casey@gmail.com>
2015-09-28 22:59:07 -0700

    First tests - inputs. Initial commit.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -1,1 +1,7 @@
 library(sear)
+context("Inputs")
+
+test_that("Inputs are checked to contain meaningful gene names", {
+  expect_warning(sear("ACTB"), "Submitted <10 genes. Enrichment may not be meaningful with so few genes as input.")
+  expect_error(sear("actb"), "Make sure input are valid gene symbols.")
+})
