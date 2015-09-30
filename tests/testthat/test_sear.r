28218c5a7689b056d4d55d48a184d556b9569a1a
Casey Shannon <also.casey@gmail.com>
2015-09-29 20:35:58 -0700

    Modify tests to reflect testing of input.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -2,6 +2,6 @@ library(sear)
 context("Inputs")
 
 test_that("Inputs are checked to contain meaningful gene names", {
-  expect_warning(sear("ACTB"), "Submitted <10 genes. Enrichment may not be meaningful with so few genes as input.")
-  expect_error(sear("actb"), "Make sure input are valid gene symbols.")
+  expect_warning(sear("ACTB"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
+  expect_error(sear("actb"), "You selected type = %s, but many of your features are not recognized.")
 })
