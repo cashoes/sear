50a5572e28e199c5a41587c5d86bd040d98d8b9f
Casey Shannon <also.casey@gmail.com>
2015-09-29 20:36:59 -0700

    Fix test to include type =.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -2,6 +2,6 @@ library(sear)
 context("Inputs")
 
 test_that("Inputs are checked to contain meaningful gene names", {
-  expect_warning(sear("ACTB"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
-  expect_error(sear("actb"), "You selected type = %s, but many of your features are not recognized.")
+  expect_warning(sear("ACTB", type = "mrna"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
+  expect_error(sear("actb", type = "mrna"), "You selected type = %s, but many of your features are not recognized.")
 })
