3bc22a159ee2b3ca8fc00b2cf11133c3fc53fe05
Casey Shannon <also.casey@gmail.com>
2015-11-02 22:48:59 -0800

    Simplify tests to assume default parameters for 'type'.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -2,6 +2,6 @@ library(sear)
 context("Inputs")
 
 test_that("Inputs are checked to contain meaningful gene names", {
-  expect_warning(sear("ACTB", type = "mrna"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
-  expect_error(sear("actb", type = "mrna"))
+  expect_warning(sear("ACTB"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
+  expect_error(sear("actb"))
 })
