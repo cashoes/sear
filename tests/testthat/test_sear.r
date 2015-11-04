3f35c76753f8e8633a8d9d74c0c5d2d2ed51e22e
Casey Shannon <also.casey@gmail.com>
2015-11-03 23:16:20 -0800

    Add test for proportion of un-matched genes.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -4,4 +4,5 @@ context("Inputs")
 test_that("Inputs are checked to contain meaningful gene names", {
   expect_warning(sear("ACTB"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
   expect_error(sear("actb"))
+  expect_error(sear(c("ACTB", "foo", "bar", "baz", "bat")))
 })
