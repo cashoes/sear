53cb08f1fd64e5531a3cda4e46f0e274d06e77f2
Casey Shannon <also.casey@gmail.com>
2015-11-05 22:06:13 -0800

    jaccard internal function.

--- R/sear.r
+++ R/sear.r
@@ -57,6 +57,10 @@ sear <- function(genes, type = c("mrna", "mirna")) {
     dplyr::ungroup(.)
 }
 
+.jaccard  <- function(s1, s2) {
+  length(intersect(s1, s2))/length(union(s1, s2))
+}
+
 .process_nodes <- function(nodes) {
   # add zero-indexed rowid column for networkD3
   nodes <- nodes %>%
