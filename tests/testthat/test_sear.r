9aa397101f924e5c00ef2e95ba2958d09efd6266
Casey Shannon <also.casey@gmail.com>
2015-11-09 21:56:33 -0800

    Chnage test to fit new, more general warning message on failed input.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -2,7 +2,7 @@ library(sear)
 context("Inputs")
 
 test_that("Inputs are checked to contain meaningful gene names", {
-  expect_warning(sear("ACTB"), "You submitted <10 valid symbols. Results may not be meaningful with so few genes.")
+  expect_warning(sear("ACTB"), "You submitted <10 valid symbols. Results may not be meaningful with so few inputs.")
   expect_error(sear("actb"))
   expect_error(sear(c("ACTB", "foo", "bar", "baz", "bat")))
 })
