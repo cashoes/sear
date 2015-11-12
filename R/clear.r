d3451326b6af46346a9d204b329c0e425596e289
Casey Shannon <also.casey@gmail.com>
2015-11-11 22:03:26 -0800

    Move internal functions to end of file.

--- R/clear.r
+++ R/clear.r
@@ -1,43 +1,3 @@
-jaccard  <- function(s1, s2) {
-  length(intersect(s1, s2))/length(union(s1, s2))
-}
-
-rebase_links <- function(nodes) {
-  ref <- nodes$members
-  names(ref) <- nodes$rowid
-  t(combn(as.numeric(names(ref)), 2)) %>%
-    as.data.frame(.) %>%
-    dplyr::tbl_df(.) %>%
-    dplyr::rename(source = V1, target = V2) %>%
-    dplyr::mutate(jaccard  = unlist(purrr::map2(ref[as.character(source)],
-                                                ref[as.character(target)],
-                                                jaccard))) %>%
-    dplyr::mutate(source = match(source, nodes$rowid) - 1,
-                  target = match(target, nodes$rowid) - 1)
-}
-
-trim_links <- function(nodes, links, cutoff) {
-    selection <- links %>%
-      dplyr::group_by(source) %>%
-      dplyr::select(node = source, jaccard) %>%
-      dplyr::summarise(n = sum(jaccard >= cutoff)) %>%
-      dplyr::arrange(desc(n)) %>%
-      dplyr::filter(n > 0) %>%
-      dplyr::first(.)
-    nodes <- nodes %>% dplyr::slice(selection + 1)
-}
-
-create_colorscale <- function(nodes, palette) {
-  cols <- RColorBrewer::brewer.pal(9, palette)[-c(1:2)]
-  # cols <- c('blue', 'purple', 'red')
-  cols <- paste0("'", paste(cols, collapse = "', '"), "'")
-  # range <- c(0, 5, 10, 50, 100, 200, -log10(.Machine$double.xmin) %>% ceiling())
-  max <- -log10(nodes$fdr) %>% max() %>% ceiling()
-  range <- quantile(0:max) %>% ceiling()
-  range <- paste(range, collapse = ", ")
-  networkD3::JS(paste0('d3.scale.linear().domain([', range, ']).range([', cols, '])'))
-}
-
 #' Clear
 #'
 #' @param leading_edge The output of running sear on an input list of mRNAs or
@@ -86,4 +46,44 @@ clear <- function(leading_edge, cutoff = 0.25, trim = FALSE) {
                           fontSize = 16, fontFamily = 'sans-serif', opacity = 0.75,
                           zoom = F, legend = T, bounded = T, opacityNoHover = 0.25,
                           charge = -300)
+}
+
+jaccard  <- function(s1, s2) {
+  length(intersect(s1, s2))/length(union(s1, s2))
+}
+
+rebase_links <- function(nodes) {
+  ref <- nodes$members
+  names(ref) <- nodes$rowid
+  t(combn(as.numeric(names(ref)), 2)) %>%
+    as.data.frame(.) %>%
+    dplyr::tbl_df(.) %>%
+    dplyr::rename(source = V1, target = V2) %>%
+    dplyr::mutate(jaccard  = unlist(purrr::map2(ref[as.character(source)],
+                                                ref[as.character(target)],
+                                                jaccard))) %>%
+    dplyr::mutate(source = match(source, nodes$rowid) - 1,
+                  target = match(target, nodes$rowid) - 1)
+}
+
+trim_links <- function(nodes, links, cutoff) {
+  selection <- links %>%
+    dplyr::group_by(source) %>%
+    dplyr::select(node = source, jaccard) %>%
+    dplyr::summarise(n = sum(jaccard >= cutoff)) %>%
+    dplyr::arrange(desc(n)) %>%
+    dplyr::filter(n > 0) %>%
+    dplyr::first(.)
+  nodes <- nodes %>% dplyr::slice(selection + 1)
+}
+
+create_colorscale <- function(nodes, palette) {
+  cols <- RColorBrewer::brewer.pal(9, palette)[-c(1:2)]
+  # cols <- c('blue', 'purple', 'red')
+  cols <- paste0("'", paste(cols, collapse = "', '"), "'")
+  # range <- c(0, 5, 10, 50, 100, 200, -log10(.Machine$double.xmin) %>% ceiling())
+  max <- -log10(nodes$fdr) %>% max() %>% ceiling()
+  range <- quantile(0:max) %>% ceiling()
+  range <- paste(range, collapse = ", ")
+  networkD3::JS(paste0('d3.scale.linear().domain([', range, ']).range([', cols, '])'))
 }
