0251e9cfd62c450ac0ba7d34ec5f0eb6afb59ee1
Casey Shannon <also.casey@gmail.com>
2023-04-24 00:00:27 -0700

    Update and clean up code to map to mirnas.

--- data-raw/3_genesets_tbl_to_mirs.r
+++ data-raw/3_genesets_tbl_to_mirs.r
@@ -18,48 +18,35 @@ tbl_mirs <- lapply(files, function(gmt){
   names(gmt) <- lapply(gmt, function(x) head(x, 1))
   gmt <- lapply(gmt, function(x) tail(x, -3))
 })
+
 # convert lists to list-columns and bind into data.frame
-tbl_mirs <- lapply(tbl_mirs, function(x) dplyr::data_frame(geneset = names(x), members = I(x))) %>%
+tbl_mirs <- lapply(tbl_mirs, function(x) dplyr::tibble(geneset = names(x), members = x)) %>%
   dplyr::bind_rows(.id = "subcollection") %>%
   dplyr::mutate(collection = 'mirwalk') %>%
   dplyr::select(collection, subcollection, geneset, members) %>%
   dplyr::as_tibble()
 
-getmirs <- function(geneset, mirs_annot) {
-  mirs_annot %>%
-    dplyr::last() %>%
-    purrr::map_int(function(x) length(intersect(x, geneset))) %>%
-    `>`(0) %>%
-    which() %>%
-    dplyr::slice(mirs_annot, .) %>%
-    dplyr::nth(3)
-}
-
-# system.time({
-#   mirs_annot %>%
-#     dplyr::last() %>%
-#     purrr::map_int(function(x) length(intersect(x, geneset))) %>%
-#     `>`(0) %>%
-#     which() %>%
-#     dplyr::slice(mirs_annot, .) %>%
-#     dplyr::nth(3)
-# })
-#
-# # this is slower and also returns partial matches... don't use
-# system.time({
-#   mirs_annot$geneset[grep(paste(geneset, collapse = "|"), mirs_annot$members)] %>%
-#     strsplit(",") %>%
-#     unlist() %>%
-#     unique()
-# })
-
 # caution, slow ~6min with 'validated' subcollection only
 mirs_annot <- tbl_mirs %>% filter(subcollection == 'validated')
 
+getmirs <- function(geneset, mirs_annot) {
+  mirs_annot %>%
+    tidyr::unnest(members) %>%
+    dplyr::filter(members %in% !!geneset) %>%
+    dplyr::distinct(geneset) %>%
+    purrr::flatten_chr()
+}
+
 system.time({
-  collections <- collections %>%
-    dplyr::rowwise(.) %>%
-    dplyr::mutate(members_mirna = list(getmirs(members_mrna, mirs_annot)))
+  library(future)
+  plan(multisession, workers = 10)
+  collections$members_mirna <- collections$members_mrna %>%
+    furrr::future_map(
+      .options = furrr::furrr_options(globals = c('mirs_annot', 'getmirs')),
+      .progress = T,
+      ~ getmirs(.x, mirs_annot)
+    )
+  plan(sequential)
 })
 
 library(ggplot2)
@@ -75,5 +62,4 @@ collections %>%
 rm(files, tbl_mirs, mirs_annot, btms, tissues, msigdb)
 
 # finally, save object
-collections <- collections %>% dplyr::ungroup()
-devtools::use_data(collections, overwrite = T)
+usethis::use_data(collections, overwrite = T)
