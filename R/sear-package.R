e500271b8484e3d9926c314c71d957fc5f417272
Casey Shannon <also.casey@gmail.com>
2023-05-01 11:44:20 -0700

    More detailed/specific imports.

---
+++ R/sear-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+#' @aliases testduplicate-package
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats filter
+#' @importFrom stats p.adjust
+#' @importFrom stats phyper
+#' @importFrom stats quantile
+#' @importFrom utils combn
+#' @importFrom utils data
+## usethis namespace: end
+NULL
