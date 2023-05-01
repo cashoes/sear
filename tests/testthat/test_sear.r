0f9dfa5569d32cfb70be7661171c58220b37071e
Casey Shannon <also.casey@gmail.com>
2023-05-01 11:46:01 -0700

    Update tests to reflect changes.

--- tests/testthat/test_sear.r
+++ tests/testthat/test_sear.r
@@ -2,10 +2,9 @@ library(sear)
 context("Inputs")
 
 test_that("Inputs are checked to contain valid symbols", {
-  expect_warning(sear("actb"))
+  expect_error(sear("actb"))
 })
 
 test_that("Inputs are checked to contain sufficient number of valid symbols", {
-  expect_warning(sear("ACTB"))
   expect_warning(sear(c("ACTB", "foo", "bar", "baz", "bat")))
 })
